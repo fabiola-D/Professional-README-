8b60569aa59e6e5ab88c06acde437c191681bfac
fabiola <fabiola-cute-@hotmail.com>
2023-10-17 18:30:51 +0100

    update

--- starter/index.js
+++ starter/index.js
@@ -5,15 +5,69 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
-
+    {
+       type:'',
+       name;'',
+       message: 'what is your Github username?'
+    },
+    {
+        type: 'input',
+        name:  'email',
+        message: 'what is your email address?'
+    },
+    {
+        type: 'input',
+        name: 'title',
+        message: "what is your project's name? " 
+    },
+    {
+        type: 'input',
+        name; 'description',
+        message: 'please write a short description of your project? '
+    },
+    {
+        type: 'list',
+        name:  'license',
+        message: 'what kind of license? ',
+        choice: ["MIT", "APACHE 2.0", "GPL","NONE"]
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        mesage: 'what command should be run to install dependencies',
+        default:'npm i'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        mesage: 'what command should be run to test ',
+        default:'npm test'
+        
+    }
+    {
+        type: 'input',
+        name: 'usage',
+        mesage: 'what does the user need to know unsing the repo ',
+        
+    }
+    {
+        type: 'input',
+        name: 'contributing',
+        mesage: 'what does the user need to know unsing the repo ',      
+    }
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data);
 }
 
 // function to initialize program
 function init() {
+    inquirer.prompt(question).then((Response) => {
+        console.log(`Generating README...`);
+        writeToFile('README.md', generateMarkdown({...response}))
+    })
 
 }
 
--- starter/utils/generateMarkdown.js
+++ starter/utils/generateMarkdown.js
@@ -1,8 +1,19 @@
 // function to generate markdown for README
+function renderLicenseBadge(license) {
+  if(license !== "None") {
+    return `![Github license](https://img.shields.io/badge/license-${license}-blue.svg)`
+  
+ }
+ return ''
+}
+
 function generateMarkdown(data) {
   return `# ${data.title}
+  ${renderLicenseBadge(data.license)}
+  
 
-`;
+  `;
+
 }
 
 module.exports = generateMarkdown;
